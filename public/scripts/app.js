622f16313b12eb9f4ab35293f2a1e790d1abd87e
Melanie <m.maddix@gmail.com>
2019-06-13 20:51:29 +0000

    adds form validation and cross-site scripting protection

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const maxTweetLength = 140;
+
 // return # of days/hours/minutes since a time
 const timeSinceUnixDate = function( unixDate ) {
-
   const currentDate = Date.now();
   const msecPassed = currentDate - unixDate;  // milliseconds
   const seconds = Math.round(msecPassed / 1000);  // ms per sec
@@ -31,13 +32,23 @@ const timeSinceUnixDate = function( unixDate ) {
   timePassed += ` ago`;
 
   return timePassed;
+}
 
+// adapted from:
+// https://www.w3resource.com/javascript-exercises/javascript-string-exercise-19.php
+function escapeHtml(unsafe) {
+  let safeStr = unsafe
+       .replace(/&/g, "&amp;")
+       .replace(/</g, "&lt;")
+       .replace(/>/g, "&gt;")
+       .replace(/"/g, "&quot;")
+       .replace(/'/g, "&#039;");
+  return(safeStr);
 }
 
 // takes in a tweet object and returns an <article>
 // containing the entire HTML structure of the tweet.
 const createTweetElement = function(tweetData) {
-
   const tweet = tweetData;
   let outputHTML = '';
 
@@ -51,7 +62,7 @@ const createTweetElement = function(tweetData) {
       <span class="user-full-name">${tweet.user.name}</span>
       <span class="user-handle">${tweet.user.handle}</span>
       </header>
-      <p>${tweet.content.text}</p>
+      <p>${escapeHtml(tweet.content.text)}</p>
       <footer class="footer grey-border-top">
         <p>${timePassed}</p>
         <span class="footer-images">
@@ -66,27 +77,45 @@ const createTweetElement = function(tweetData) {
   }
 
   return outputHTML;
+}
 
+const sortTweetsByDateDesc = function (tweets) {
+  // no extra array variable required
+  tweets.sort(function(a, b) {
+    return parseFloat(b.created_at) - parseFloat(a.created_at);
+  });
 }
 
 const renderTweets = function(tweets) {
-
   let arrTweets = [];
 
+  sortTweetsByDateDesc(tweets);
   for( let tweet in tweets) {
     arrTweets.push(createTweetElement(tweets[tweet]));
   }
 
   return arrTweets;
+}
 
+const checkTweetLength = function(tweet) {
+  const tweetLength = tweet.length;
+  let response = '';
+
+  if( tweetLength === 0 ) {
+    response = 'Please enter some text.';
+  } else if (tweetLength > maxTweetLength ) {
+    response = 'Your tweet is too long. Please delete some characters';
+  }
+
+  return(response);
 }
 
 $(document).ready(function() {
 
-  // load tweets
   const loadTweets = function() {
     $.ajax('/tweets', { method: 'GET' })
       .then(function (loadedTweets) {
+        $('#tweets-container').html('');
         $('#tweets-container').append(renderTweets(loadedTweets));
     });
   }
@@ -97,17 +126,30 @@ $(document).ready(function() {
   $tweetForm.addEventListener('submit', function(ev) {
     ev.preventDefault();
 
-    $.ajax({
-      url:    '/tweets',
-      method: 'POST',
-      data:   $(this).serialize()
-    })
-    .done(function() {
-      console.log(`success`);
-    })
-    .fail(function(err) {
-      console.log( `error: ${err.statusText}` );
-    });
+    // check for invalid tweet length
+    let responseMsg = checkTweetLength($('textarea').val());
+    const responseWindow = $(this).children("#tweet-response-window");
+    responseWindow.text(responseMsg);
+
+    if ( responseMsg ) {
+      responseWindow.addClass("tweet-err");
+    } else {
+      responseWindow.removeClass("tweet-err");
+
+      // submit the form
+      $.ajax({
+        url:    '/tweets',
+        method: 'POST',
+        data:   $(this).serialize()
+      })
+      .done(function() {
+        loadTweets();
+
+      })
+      .fail(function(err) {
+        console.log( `error: ${err.statusText}` );
+      });
+    }
 
   });
 
